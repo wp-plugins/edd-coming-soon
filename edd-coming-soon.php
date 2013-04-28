7e3fa8b43894ff76c0edb417c7d7137e237bd787
sumobi <sumobi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-28 04:51:16 +0000

    Added the ability to set custom text per download
    git-svn-id: https://plugins.svn.wordpress.org/edd-coming-soon/trunk@704875 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edd-coming-soon.php
+++ edd-coming-soon.php
@@ -1,132 +1,189 @@
 <?php
 /*
 Plugin Name: EDD Coming Soon
-Plugin URI: http://sumobi.com/store/edd-download-images/
-Description: Allows "coming soon" downloads in Easy Digital Downloads.
-Version: 1.0
-Author: Andrew Munro - Sumobi
-Author URI: http://sumobi.com
+Plugin URI: http://sumobi.com/store/edd-coming-soon/
+Description: Allows "custom status" downloads (not available for purchase) in Easy Digital Downloads
+Version: 1.1
+Author: Sumobi
+Author URI: http://sumobi.com/
+Contributors: sc0ttkclark
 License: GPL-2.0+
 License URI: http://www.opensource.org/licenses/gpl-license.php
 */
 
+// Plugin constants
+if ( !defined( 'EDD_COMING_SOON' ) )
+	define( 'EDD_COMING_SOON', '1.1' );
+
+if ( !defined( 'EDD_COMING_SOON_URL' ) )
+	define( 'EDD_COMING_SOON_URL', plugin_dir_url( __FILE__ ) );
+
+if ( !defined( 'EDD_COMING_SOON_DIR' ) )
+	define( 'EDD_COMING_SOON_DIR', plugin_dir_path( __FILE__ ) );
 
 /**
  * Internationalization
- * @since 1.0 
+ *
+ * @since 1.0
  */
-function edd_cs_textdomain() {
-	load_plugin_textdomain( 'edd-di', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+function edd_coming_soon_textdomain () {
+	load_plugin_textdomain( 'edd-coming-soon', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action( 'init', 'edd_cs_textdomain' );
+add_action( 'init', 'edd_coming_soon_textdomain' );
 
+/**
+ * Check if it's a Custom Status download
+ *
+ * @param int $download_id Download Post ID
+ *
+ * @return boolean Whether Custom Status is active
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_is_active ( $download_id = 0 ) {
+	global $post;
 
-/**		
- * Render the coming soon checkbox
- * @since 1.0 
-*/
-function edd_cs_render_option( $post_id ) { 
+	if ( empty( $download_id ) && is_object( $post ) && isset( $post->ID ) )
+		$download_id = $post->ID;
 
-	$coming_soon = get_post_meta( $post_id, 'edd_coming_soon', true );
+	if ( !empty( $download_id ) )
+		return (boolean) get_post_meta( $download_id, 'edd_coming_soon', true );
 
-	?>
+	return false;
+}
+
+/**
+ * Render the Custom Status checkbox
+ *
+ * @param int $post_id Post ID
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_render_option ( $post_id ) {
+	$coming_soon = (boolean) get_post_meta( $post_id, 'edd_coming_soon', true );
+	$coming_soon_text = get_post_meta( $post_id, 'edd_coming_soon_text', true );
+
+	// Default
+	$default_text = apply_filters( 'edd_cs_coming_soon_text', __( 'Coming Soon', 'edd-coming-soon' ) );
+?>
 	<p>
 		<label for="edd_coming_soon">
-			<input type="checkbox" name="edd_coming_soon" id="edd_coming_soon" value="1" <?php checked( 1, $coming_soon ); ?> />
-			<?php echo apply_filters( 'edd_cs_toggle_text', __( 'Enable coming soon download', 'edd-cs' ) ); ?>
+			<input type="checkbox" name="edd_coming_soon" id="edd_coming_soon" value="1" <?php checked( true, $coming_soon ); ?> />
+			<?php _e( 'Enable Coming Soon / Custom Status download', 'edd-coming-soon' ); ?>
 		</label>
 	</p>
 
-<?php }
-add_action( 'edd_meta_box_fields', 'edd_cs_render_option', 10 );
+	<p id="edd_coming_soon_container"<?php echo ( $coming_soon ? '' : ' style="display:none;"' ); ?>>
+		<label for="edd_coming_soon_text">
+			<input type="text" name="edd_coming_soon_text" id="edd_coming_soon_text" size="45" style="width:110px;" value="<?php echo esc_attr( $coming_soon_text ); ?>" />
+			<?php echo sprintf( __( 'Custom Status text (default: <em>%s</em>)', 'edd-coming-soon' ), $default_text ); ?>
+		</label>
+	</p>
+<?php
+}
+add_action( 'edd_meta_box_fields', 'edd_coming_soon_render_option', 10 );
 
-
-/**		
+/**
  * Hook into save filter and add the download image fields
- * @since 1.0 
-*/
-function edd_cs_metabox_fields_save( $fields ) {
-
+ *
+ * @param array $fields Array of fields to save for EDD
+ *
+ * @return array Array of fields to save for EDD
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_metabox_fields_save ( $fields ) {
 	$fields[] = 'edd_coming_soon';
+	$fields[] = 'edd_coming_soon_text';
 
 	return $fields;
 }
-add_filter( 'edd_metabox_fields_save', 'edd_cs_metabox_fields_save' );
+add_filter( 'edd_metabox_fields_save', 'edd_coming_soon_metabox_fields_save' );
 
+/**
+ * Hook into currency filter and if the download is a Custom Status download, return no currency
+ *
+ * @return string The currency code to be used
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_edd_currency ( $currency ) {
+	if ( edd_coming_soon_is_active() )
+		return '';
 
-/**		
- * Check if it's a coming soon download
- * @since 1.0 
-*/
-function edd_cs_is_coming_soon() {
+	return $currency;
+}
+add_filter( 'edd_currency', 'edd_coming_soon_edd_currency' );
 
+/**
+ * Filter price function so it shows Custom Status text instead of price
+ *
+ * @param string $price Price text
+ * @param int $download_id Download Post ID
+ *
+ * @return string Price text to be shown
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_filter_price ( $price, $download_id ) {
+	if ( edd_coming_soon_is_active( $download_id ) ) {
+		// Default
+		$coming_soon_text = apply_filters( 'edd_cs_coming_soon_text', __( 'Coming Soon', 'edd-coming-soon' ) );
+
+		// Custom override
+		$custom_text = get_post_meta( $download_id, 'edd_coming_soon_text', true );
+
+		if ( 0 < strlen( $custom_text ) )
+			$coming_soon_text = $custom_text;
+
+		return apply_filters( 'edd_coming_soon_text', $coming_soon_text, $download_id );
+	}
+
+	return $price;
+}
+add_filter( 'edd_download_price', 'edd_coming_soon_filter_price', 10, 2 );
+
+/**
+ * Only show purchase form if it's not a Custom Status download
+ *
+ * @param string $purchase_form Form HTML
+ * @param array $args Arguments for display
+ *
+ * @return string Form HTML
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_purchase_download_form ( $purchase_form, $args ) {
+	if ( edd_coming_soon_is_active( $args[ 'download_id' ] ) )
+		return '';
+
+	return $purchase_form;
+}
+add_filter( 'edd_purchase_download_form', 'edd_coming_soon_purchase_download_form', 10, 2 );
+
+/**
+ * Prevent download from being added to cart (free or priced) with ?edd_action=add_to_cart&download_id=XXX
+ *
+ * @param int $download_id Download Post ID
+ *
+ * @since 1.0
+ */
+function edd_coming_soon_pre_add_to_cart ( $download_id ) {
+	if ( edd_coming_soon_is_active( $download_id ) ) {
+		$add_text = apply_filters( 'edd_coming_soon_pre_add_to_cart', __( 'This download cannot be purchased', 'edd-coming-soon' ), $download_id );
+
+		wp_die( $add_text, '', array( 'back_link' => true ) );
+	}
+}
+add_action( 'edd_pre_add_to_cart', 'edd_coming_soon_pre_add_to_cart' );
+
+function edd_coming_soon_admin_scripts ( $hook ) {
 	global $post;
 
-	$coming_soon = isset( $post->ID ) ? get_post_meta( $post->ID, 'edd_coming_soon', true ) : '';
-
-	if( $coming_soon )
-		return true;
-
-}
-
-
-/**		
- * Hook into currency filter and if the download is a coming soon download, return no currency
- * @since 1.0 
-*/
-function edd_cs_edd_currency( $currency ) {
-	
-	if( edd_cs_is_coming_soon() )
-		return false;
-	else
-		return $currency;
-	
-}
-add_filter( 'edd_currency', 'edd_cs_edd_currency' );
-
-
-/**		
- * Filter price function so it shows 'Coming Soon' instead of price
- * @since 1.0 
-*/
-function edd_cs_filter_price( $price, $download_id ) {
-
-	if ( edd_cs_is_coming_soon() ) {
-		$price = __( 'Coming Soon', 'edd-cs' );
-		return apply_filters( 'edd_cs_coming_soon_text', $price );
+	if ( is_object( $post ) && $post->post_type != 'download' ) {
+		return;
 	}
-	else 
-		return $price;
 
+	wp_enqueue_script( 'edd-cp-admin-scripts', EDD_COMING_SOON_URL . 'js/edd-coming-soon-admin.js', array( 'jquery' ), EDD_COMING_SOON );
 }
-add_filter( 'edd_download_price', 'edd_cs_filter_price', 10, 2 );
-
-
-/**		
- * Only show purchase form if it's not a coming soon download
- * @since 1.0
- * @todo override purchase_link shortcode also
-*/
-function edd_cs_purchase_download_form( $purchase_form, $args ) {
-
-	if( edd_cs_is_coming_soon() ) {
-		return false;
-	}
-	else
-		return $purchase_form;
-}
-add_filter( 'edd_purchase_download_form', 'edd_cs_purchase_download_form', 10, 2 );
-
-
-/**		
- * Prevent download from being added to cart (free or priced) with ?edd_action=add_to_cart&download_id=XXX
- * @since 1.0 
-*/
-
-function edd_cs_pre_add_to_cart( $download_id ) {
-
-	if( get_post_meta( $download_id, 'edd_coming_soon', true ) )
-		wp_die( apply_filters( 'edd_cs_pre_add_to_cart', __( 'This download cannot be purchased', 'edd-cs' ) ), '', 'back_link=true' );
-
-}
-add_action( 'edd_pre_add_to_cart', 'edd_cs_pre_add_to_cart' );
+add_action( 'admin_enqueue_scripts', 'edd_coming_soon_admin_scripts' );
--- readme.txt
+++ readme.txt
@@ -1,45 +1,45 @@
 === EDD Coming Soon ===
-Contributors: sumobi
+Contributors: sumobi, sc0ttkclark
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EFUPMPEZPGW7L
 Tags: easy digital downloads, digital downloads, e-downloads, edd, coming soon, sumobi
 Requires at least: 3.3
-Tested up to: 3.5.1
-Stable tag: 1.0
+Tested up to: 3.6
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Allows "coming soon" downloads in Easy Digital Downloads.
+Allows Coming Soon or Custom Status text instead of normal pricing for downloads in Easy Digital Downloads.
 
 == Description ==
 
-This plugin requires [Easy Digital Downloads](http://wordpress.org/extend/plugins/easy-digital-downloads/ "Easy Digital Downloads"). It allows downloads to be "coming soon", and prevents them from being added to the cart. It does a few things:
+This plugin requires [Easy Digital Downloads](http://wordpress.org/extend/plugins/easy-digital-downloads/ "Easy Digital Downloads"). It allows downloads to be "Coming Soon" or have Custom Status text, and prevents them from being added to the cart. It does a few things:
 
-1. Adds a checkbox to the download configuration so you can set the download to "coming soon".
-1. Replaces the download's price in the pricing admin column with "coming soon".
-1. Displays “Coming soon” instead of the price when using the [downloads] shortcode, and anywhere else where the edd_price() function has been called.
+1. Adds a checkbox to the download configuration so you can set the download to Coming Soon / Custom Status.
+1. Adds a text field to the download configuration so you can set the text to show (default "Coming Soon").
+1. Replaces the download's price in the pricing admin column with "Coming Soon" or the Custom Status text.
+1. Displays "Coming Soon" or the Custom Status text instead of the price when using the [downloads] shortcode, and anywhere else where the edd_price() function has been called.
 1. Prevents the coming soon download from being purchased. The plugin will remove the purchase button and stop the download from being added to cart via the edd_action. Eg ?edd_action=add_to_cart&download_id=XXX
-
-= Filtering the coming soon text =    
-
-Paste this into your functions.php and modify the text below:
-
-    function themename_coming_soon_text() { 
-	    return 'Available Soon';
-    }
-    add_filter( 'edd_cs_coming_soon_text', 'themename_coming_soon_text' );
 
 
 == Installation ==
 
-1. Upload entire `edd-coming soon` folder to the `/wp-content/plugins/` directory, or just upload the ZIP package via 'Plugins > Add New > Upload' in your WP Admin
-1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Mark downloads as "coming soon"
+1. Unpack the entire contents of this plugin zip file into your `wp-content/plugins/` folder locally
+1. Upload to your site
+1. Navigate to `wp-admin/plugins.php` on your site (your WP Admin plugin page)
+1. Activate this plugin
+
+OR you can just install it with WordPress by going to Plugins >> Add New >> and type this plugin's name
+
+Then proceed to mark downloads as "Coming Soon".
 
 == Screenshots ==
 
 1. Download Configuration metabox with new coming soon checkbox option
 
 == Changelog ==
+
+= 1.1 =
+* Added the ability to set custom text per download, default remains "Coming Soon". Thanks to @sc0ttkclark
 
 = 1.0 =
 * Initial release
